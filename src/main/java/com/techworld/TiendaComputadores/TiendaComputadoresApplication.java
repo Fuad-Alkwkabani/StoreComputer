4592cd9454eba577ffcacada60533e4a135c2ccc
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-12-18 20:39:59 +0100

    first commit

---
+++ src/main/java/com/techworld/TiendaComputadores/TiendaComputadoresApplication.java
@@ -0,0 +1,13 @@
+package com.techworld.TiendaComputadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaComputadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaComputadoresApplication.class, args);
+	}
+
+}
