cc91452dc37cc35d6f9bcfb74b805a7dc7a9b3a9
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-12-18 23:17:31 +0100

    craet the entities

--- src/main/java/com/techworld/TiendaComputadores/model/Computer.java
+++ src/main/java/com/techworld/TiendaComputadores/model/Computer.java
@@ -1,5 +1,22 @@
 package com.techworld.TiendaComputadores.model;
 
-public class Computer {
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+@Data
+public class Computer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String brand;
+    private int memory; // GB
+    private String processor;
+    private String operatingSystem;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
 }
--- src/main/java/com/techworld/TiendaComputadores/model/Store.java
+++ src/main/java/com/techworld/TiendaComputadores/model/Store.java
@@ -1,5 +1,20 @@
 package com.techworld.TiendaComputadores.model;
 
-public class Store {
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
 
+@Entity
+@Data
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String owner;
+    private String taxId;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Computer> inventory;
 }
