8e5051cae72c8e14e68aab9266d902503c541a70
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-12-18 23:23:20 +0100

    creat repo

--- src/main/java/com/techworld/TiendaComputadores/repository/ComputerRepository.java
+++ src/main/java/com/techworld/TiendaComputadores/repository/ComputerRepository.java
@@ -1,5 +1,8 @@
 package com.techworld.TiendaComputadores.repository;
 
-public class ComputerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.techworld.TiendaComputadores.model.Computer;
+
+public interface ComputerRepository extends JpaRepository<Computer, Long> {
 }
--- src/main/java/com/techworld/TiendaComputadores/repository/StoreRepository.java
+++ src/main/java/com/techworld/TiendaComputadores/repository/StoreRepository.java
@@ -1,5 +1,8 @@
 package com.techworld.TiendaComputadores.repository;
 
-public class StoreRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.techworld.TiendaComputadores.model.Store;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
 }
